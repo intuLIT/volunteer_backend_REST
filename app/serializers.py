cdf11123e79d1eae5631d8bff02fbca27baaabba
Jonathan Molina <nemolina@calpoly.edu>
2016-10-23 01:31:10 -0700

    get user by email working

--- app/serializers.py
+++ app/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from app.models import *
 
+# Model Serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'name', 'email', 'phone', 'location',)
+
+# Serializers
+class EmailSerialzer(serializers.Serializer):
+    email = serializers.CharField(max_length=100)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app.models import User
-from app.serializers import UserSerializer
+from app.serializers import *
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -7,13 +7,13 @@ from rest_framework import status
 
 
 class UserDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return User.objects.get(pk=pk)
-        except User.DoesNotExist:
-            raise Http404
+    def post(self, request, format=None):
+        serializer = EmailSerialzer(data=request.data)
+        if serializer.is_valid():
+            user = User.objects.get(email=serializer.email)
+            serializer = UserSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
 
-    def get(self, request, pk, format=None):
-        user = self.get_object(pk)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
+
