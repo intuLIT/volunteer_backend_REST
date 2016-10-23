a76a09ff2a4701f02427bde4fa35097efe96a1df
Jonathan Molina <nemolina@calpoly.edu>
2016-10-23 12:17:13 -0700

    wtf

--- app/views.py
+++ app/views.py
@@ -4,7 +4,6 @@ from django.http import Http404, JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from app.fpauth import *
 
 from itertools import chain
 import json
